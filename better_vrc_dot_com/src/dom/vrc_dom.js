0ff408f3e59c9ff4e3791b645fa2eda64392448c
riku1227 <17927874+riku1227@users.noreply.github.com>
2022-09-11 14:10:27 +0900

    :sparkles: Add update instance user button

--- better_vrc_dot_com/src/dom/vrc_dom.js
+++ better_vrc_dot_com/src/dom/vrc_dom.js
@@ -12,7 +12,7 @@ class VRCDOM {
         //親SVG要素を作る
         const iconSVG = document.createElementNS("http://www.w3.org/2000/svg","svg");
         //必要なクラスを追加
-        DOM.addClassList(iconSVG, ["svg-inline--fa", "ms-2"]);
+        DOM.addClassList(iconSVG, ["svg-inline--fa"]);
         //viewBoxを設定
         iconSVG.setAttribute("viewBox", viewBox);
 
@@ -44,8 +44,46 @@ class VRCDOM {
         const badgeDiv = document.createElement("div");
         DOM.addClassList(badgeDiv, ["align-self-end", "me-0", "me-sm-2", "align-items-center", "css-jm7yu6"]);
         badgeDiv.textContent = content;
+        DOM.addClassList(svgIcon, ["ms-2"]);
         badgeDiv.appendChild(svgIcon);
 
         return badgeDiv;
+    }
+
+    /**
+     * ボタンを生成する
+     * @param {String} label 
+     * @param {Function} onClick 
+     */
+    static createButton(label, onClick) {
+        const button = document.createElement("button");
+        //ボタンをボタンたらしめるクラスを追加
+        DOM.addClassList(button, ["align-self-end", "css-21vcfd"]);
+        //ボタンのテキストを設定
+        button.textContent = label;
+        //クリックイベントを設定
+        button.addEventListener("click", (event) => { onClick(event) });
+
+        return button;
+    }
+
+    /**
+     * アイコンボタンを生成する
+     * @param {Element} svgIcon 
+     * @param {Function} onClick 
+     * @returns 
+     */
+    static createIconButton(svgIcon, onClick) {
+        const button = document.createElement("button");
+        //ボタンをボタンたらしめるクラスを追加
+        DOM.addClassList(button, ["align-self-end", "css-21vcfd"]);
+        //中身のアイコンにマージンを追加
+        DOM.addClassList(svgIcon, ["mx-1"]);
+        //ボタンにアイコンを追加
+        button.appendChild(svgIcon);
+        //クリックイベントを設定
+        button.addEventListener("click", (event) => { onClick(event) });
+
+        return button;
     }
 }
--- better_vrc_dot_com/src/features/better_location_card.js
+++ better_vrc_dot_com/src/features/better_location_card.js
@@ -6,11 +6,20 @@ class BetterLocationCard {
 
     static friendsCountPClass = "better_vrc_dot_com_location_friends_count_p";
     static instanceUserCountBadgeClass = "better_vrc_dot_com_instance_user_count_badge";
+    static instanceUserCountUpdateButtonClass = "better_vrc_dot_com_instance_user_count_update_button";
 
+    //Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc.
     static fasHouseUserPath = "M576 287.6H511.8l1 224.4H64.1V287.6H0V240L288.4 0 576 240v47.6zM352 224c0-35.3-28.7-64-64-64s-64 28.7-64 64s28.7 64 64 64s64-28.7 64-64zm48 192l-32-96H208l-32 96H400z";
     static fasHouseUserViewBox = "0 0 576 512";
 
+    //Font Awesome Pro 6.2.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc.
+    static fasRotateRightPath = "M463.5 224H472c13.3 0 24-10.7 24-24V72c0-9.7-5.8-18.5-14.8-22.2s-19.3-1.7-26.2 5.2L413.4 96.6c-87.6-86.5-228.7-86.2-315.8 1c-87.5 87.5-87.5 229.3 0 316.8s229.3 87.5 316.8 0c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0c-62.5 62.5-163.8 62.5-226.3 0s-62.5-163.8 0-226.3c62.2-62.2 162.7-62.5 225.3-1L327 183c-6.9 6.9-8.9 17.2-5.2 26.2s12.5 14.8 22.2 14.8H463.5z"
+    static fasRotateRightViewBox = "0 0 512 512";
+
     static instanceUserBadgeRegex = /([0-9]|[1-9][0-9]|[1-9][0-9][0-9]|[1-9][0-9][0-9][0-9]|Click)<svg/g;
+
+    //リロードボタンを連続でクリックし、VRC側に負荷がかかりすぎない用のクールタイム
+    static updateCoolTime = 0;
 
     /**
      * ロケーションカードにインスタンス人数を表示するバッジが存在するかどうか
@@ -18,6 +27,16 @@ class BetterLocationCard {
      */
     static existsInstanceUserCountBadge(locationCard) {
         const htmlCollection = locationCard.instanceInfoListElement.getElementsByClassName(this.instanceUserCountBadgeClass);
+        return htmlCollection.length > 0;
+    }
+
+    /**
+     * ロケーションカードにインスタンス人数を更新するボタンが存在するかどうか
+     * @param {VRCLocationCard} locationCard 
+     * @returns 
+     */
+    static existsInstanceUserCountUpdateButton(locationCard) {
+        const htmlCollection = locationCard.instanceInfoListElement.getElementsByClassName(this.instanceUserCountUpdateButtonClass);
         return htmlCollection.length > 0;
     }
 
@@ -50,9 +69,16 @@ class BetterLocationCard {
                 locationCard.instanceInfoListElement.lastChild
             );
 
-            instanceUserCountBadge.addEventListener("click", () => {
+            //バッジが"Click"状態の時にクリックしたら実行される
+            const firstUpdate = () => { 
+                //人数を表示 (取得)
                 this.updateInstanceUserCountBadge(locationCard);
-            });
+                //更新ボタンを追加
+                this.addInstanceUserCountUpdateButton(locationCard);
+                //以降バッジをクリックしてもこれが実行されないように削除する
+                instanceUserCountBadge.removeEventListener("click", firstUpdate);
+            }
+            instanceUserCountBadge.addEventListener("click", firstUpdate);
         }
     }
 
@@ -74,12 +100,46 @@ class BetterLocationCard {
     }
 
     /**
+     * インスタンスに居るユーザ数を更新するボタンを追加する
+     * @param {VRCLocationCard} locationCard 
+     */
+    static addInstanceUserCountUpdateButton(locationCard) {
+        //既に更新ボタンが追加されていたら実行しない
+        if(!this.existsInstanceUserCountUpdateButton(locationCard)) {
+            //更新ボタンをアイコン
+            const rotateRightIconSVG = VRCDOM.createSVGIcon("Update number of users in the Instance", this.fasRotateRightPath, "#FFFFFF", this.fasRotateRightViewBox);
+            //更新ボタン
+            const updateButton = VRCDOM.createIconButton(rotateRightIconSVG, (e) => { 
+                //前回の更新から10秒以上経過していたら (500msに一回カウントが追加される)
+                if(this.updateCoolTime >= 10) {
+                    this.updateInstanceUserCountBadge(locationCard);
+                    this.updateCoolTime = 0;
+                }
+             });
+            DOM.addClassList(updateButton, [ "me-sm-2", this.instanceUserCountUpdateButtonClass ]);
+
+            //Invite Meボタンの左にバッジを設置する
+            locationCard.instanceInfoListElement.insertBefore(
+                updateButton,
+                locationCard.instanceInfoListElement.lastChild
+            );
+        }
+    }
+
+    /**
      * ベターロケーションカードのセットアップを行う
      * ベターロケーションカードを使うにはこの処理を実行するればいい
      */
     static setupBetterLocationCard() {
+        this.updateCoolTime++;
+        //ロケーションカードが入ってるdiv要素
+        const locationCardList = DOM.getByClass(this.locationsDivClassName);
+        //存在しなかったら実行しない
+        if(locationCardList.length == 0) {
+            return;
+        }
         //ロケーションカードをすべて取得する
-        const locationCards = DOM.getByClass(this.locationsDivClassName)[0].children;
+        const locationCards = locationCardList[0].children;
         //forで回す
         for(let i = 0; i < locationCards.length; i++) {
             //ロケーションカードインスタンスを作成
